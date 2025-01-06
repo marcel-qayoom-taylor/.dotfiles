39748ef300133254c12abfb8260ead6ba0878ff6
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2025-01-06 11:34:53 +1100

    multi monitor setup

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -6,8 +6,10 @@ FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
 echo "FOCUSED WORKSPACE IS $FOCUSED_WORKSPACE"
 
-if [ "1" = "$FOCUSED_WORKSPACE" ]; then
+if [ "$1" = $(aerospace list-monitors --focused | awk '{print $1}') ]; then
     sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME label=$FOCUSED_WORKSPACE
 else
     sketchybar --set $NAME background.drawing=off
 fi
+
--- .config/sketchybar/plugins/current_space.sh
+++ .config/sketchybar/plugins/current_space.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 update_space() {
+    # EVENTUALLY DO MONITOR BASED WORKSPACE NUM USING aerospace list-workspaces --monitor $monitor --visible
     SPACE_ID=$(aerospace list-workspaces --focused)
     echo "Workspace changed to $AEROSPACE_FOCUSED_WORKSPACE"
     echo "Current space is $SPACE_ID"
